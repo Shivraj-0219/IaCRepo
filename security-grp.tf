6882524192dae4a482a34f2319e8546d23aeb2dc
SHIVRAJ MUTTAGI <57551826+Shivraj-0219@users.noreply.github.com>
2022-04-23 12:34:49 +0530

    Angular web Calculator Security Group Updated

--- security-grp.tf
+++ security-grp.tf
@@ -1,15 +1,15 @@
 #SECURITY GROUP
-resource "aws_security_group" "hdfc-web-sg" {
-  name        = "hdfc-web-sg"
+resource "aws_security_group" "calc-web-sg" {
+  name        = "calc-web-sg"
   description = "Allow SSH/HTTP Inbound Traffic"
-  vpc_id      = aws_vpc.hdfc.id
+  vpc_id      = aws_vpc.calc.id
 
   ingress {
     description      = "SSH From WWW"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    #cidr_blocks      = [aws_vpc.hdfc.cidr_block]
+    #cidr_blocks      = [aws_vpc.calc.cidr_block]
     cidr_blocks      = ["0.0.0.0/0]
     
   }
@@ -19,12 +19,12 @@ resource "aws_security_group" "hdfc-web-sg" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    #cidr_blocks      = [aws_vpc.hdfc.cidr_block]
+    #cidr_blocks      = [aws_vpc.calc.cidr_block]
     cidr_blocks      = ["0.0.0.0/0]
     
   }
   
   tags = {
-    Name = "HDFC-WEB-SG"
+    Name = "CALC-WEB-SG"
   }
 }
