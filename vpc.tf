8c8461944f387289d84bd18cc1b490588ef92d0f
SHIVRAJ MUTTAGI <57551826+Shivraj-0219@users.noreply.github.com>
2022-04-23 12:31:31 +0530

    Angular Web Calculator VPC Infrastructure Updated

--- vpc.tf
+++ vpc.tf
@@ -4,82 +4,82 @@ variable "aws_secret_key" {}
 
 #AWS as Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
   access_key=var.aws_access_key
   secret_key=var.aws_secret_key
 }
 
 #VPC Resources
-resource "aws_vpc" "hdfc" {
+resource "aws_vpc" "calc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   enable_dns_hostnames="true"
 
   tags = {
-    Name = "HDFC-VPC"
+    Name = "CALC-VPC"
   }
 }
 
 #PUBLIC SUBNET
-resource "aws_subnet" "hdfc-pub" {
-  vpc_id     = aws_vpc.hdfc.id
-  cidr_block = "10.0.0.0/24"
+resource "aws_subnet" "calc-pub" {
+  vpc_id     = aws_vpc.calc.id
+  cidr_block = "10.0.0.0/22"
   map_public_ip_on_launch="true"
   tags = {
-    Name = "HDFC-PUB-SN"
+    Name = "CALC-PUB-SN"
   }
 }
 
 #PRIVATE SUBNET
 resource "aws_subnet" "hdfc-pvt" {
-  vpc_id     = aws_vpc.hdfc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.calc.id
+  cidr_block = "10.0.4.0/22"
   
   tags = {
-    Name = "HDFC-PVT-SN"
+    Name = "CALC-PVT-SN"
   }
 }
 
 #INTERNET GATEWAY
-resource "aws_internet_gateway" "hdfc-igw" {
-  vpc_id = "${aws_vpc.hdfc.id}"
+resource "aws_internet_gateway" "calc-igw" {
+  vpc_id = "${aws_vpc.calc.id}"
 
   tags = {
-    Name = "HDFC-IGW"
+    Name = "CALC-IGW"
   }
 }
 
 #PUBLIC ROUTE TABLE
-resource "aws_route_table" "hdfc-pub-rt" {
-  vpc_id = aws_vpc.hdfc.id
+resource "aws_route_table" "calc-pub-rt" {
+  vpc_id = aws_vpc.calc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.hdfc-igw.id
+    gateway_id = aws_internet_gateway.calc-igw.id
   }
 
   tags = {
-    Name = "HDFC-PUB-RT"
+    Name = "CALC-PUB-RT"
   }
 }
 
 #PRIVATE ROUTE TABLE
-resource "aws_route_table" "hdfc-pvt-rt" {
-  vpc_id = aws_vpc.hdfc.id
+resource "aws_route_table" "calc-pvt-rt" {
+  vpc_id = aws_vpc.calc.id
 
   tags = {
-    Name = "HDFC-PVT-RT"
+    Name = "CALC-PVT-RT"
   }
 }
 
 #PUBLIC ASSOCIATION
-resource "aws_route_table_association" "pdfc-pub-asc" {
-  subnet_id      = aws_subnet.hdfc-pub.id
-  route_table_id = aws_route_table.hdfc-pub-rt.id
+resource "aws_route_table_association" "calc-pub-asc" {
+  subnet_id      = aws_subnet.calc-pub.id
+  route_table_id = aws_route_table.calc-pub-rt.id
 }
 
 #PRIVATE ASSOCIATION
-resource "aws_route_table_association" "pdfc-pvt-asc" {
-  subnet_id      = aws_subnet.hdfc-pvt.id
-  route_table_id = aws_route_table.hdfc-pvt-rt.id
+resource "aws_route_table_association" "calc-pvt-asc" {
+  subnet_id      = aws_subnet.calc-pvt.id
+  route_table_id = aws_route_table.calc-pvt-rt.id
 }
